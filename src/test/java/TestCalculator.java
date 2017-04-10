2fd0102082aef1a7aeff223feed6b36159c190cf
Oskar Pol <oskarbank@gmail.com>
2017-04-10 18:52:00 +0200

    Added annotations

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,6 +6,27 @@ import static org.junit.Assert.assertEquals;
  * Created by OskarPraca on 2017-04-10.
  */
 public class TestCalculator {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // ta metoda wywoła się tylko raz, przed wszystkimi testami
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        // ta metoda wywoła się tylko raz, po wszystkich testach
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // metoda która wywoła się przed każdym testem
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // ta metoda pójdzie po kazdym wykonanym teście
+    }
+
     @Test
     public void testDivide() {
          Main main = new Main();
@@ -18,5 +39,6 @@ public class TestCalculator {
         Main main = new Main();
         assertEquals("Wynikiem powinno być 15", 15,  main.add(10, 5), 0);
         assertEquals("Wynikiem powinno być 18", 18,  main.add(15, 3), 0);
+
     }
 }
